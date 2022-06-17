e1e42f448daa755132526721aa51527d215b6573
Mauricio <jourdanmauricio@gmail.com>
2022-06-17 04:14:59 -0300

    contact-form added

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,10 @@
 import scrollToButton from "./boton_scroll.js";
+import contactForm from "./contact_form.js";
 
 const d = document;
 
 /* Cuando finaliza load */
 d.addEventListener("DOMContentLoaded", (e) => {
   scrollToButton(".scroll-top-btn");
+  contactForm();
 });
